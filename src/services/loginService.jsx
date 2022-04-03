06765a78f8c96835fd183141c5ca05eb2ff1997c
pepcerda <josep.cerda.roscar@gmail.com>
2022-04-03 22:25:30 +0200

    Se añade URL del API del enunciado del ejercicio

--- src/services/loginService.jsx
+++ src/services/loginService.jsx
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-//Para utilizar el API del ejercicio, cambiar esta URL por: https://three-points.herokuapp.com. No se ha utilizado por problemas con el CORS en el backend. 
-const baseURL = "https://e93e896a-53fb-4119-89cc-2697a142725d.mock.pstmn.io";
+
+const baseURL = "https://three-points.herokuapp.com";
 
 export const login = (body) => {
     console.log("Elemento de autenticación: " + body.username + " y " + body.password);
     let axiosConfig =
     {
         headers: {
-            'Content-Type': "application/json",
-            'X-Api-Key': 'PMAK-6249cad8f0b3df3cb35f9839-c52d45d6848166a9ad36b2d52194df344c'
+            'Content-Type': "application/json"
         }
     }
 
